aa600e5abfb1087b3c2876e885d7ce150df9561c
Léo Salé <leo.sale72@gmail.com>
2020-07-23 10:44:03 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Button, Typography, IconButton, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles'
+import { green, amber } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+
+  palette: {
+    primary: {
+      main: '#1b5e20',
+    },
+    secondary: {
+      main: '#00897b',
+    },
+  },
+
+})
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles(theme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+
+          <Typography variant="h6" className={classes.title}>
+            News
+    </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+
+
+      <Button variant="contained" color="secondary">
+        Clique sur moi !
+    </Button>
+    </ThemeProvider>
   );
 }
 
